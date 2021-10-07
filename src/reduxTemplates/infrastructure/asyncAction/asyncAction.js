0845f16207cbd37b47335c88b51ff3dc529521d9
Samar Saluja <samar.saluja@vicesoftware.com>
2021-10-07 12:17:53 +0530

    Eslint, Prettier, on Change added to react, node boilerplate

--- src/reduxTemplates/infrastructure/asyncAction/asyncAction.js
+++ src/reduxTemplates/infrastructure/asyncAction/asyncAction.js
@@ -1,49 +1,45 @@
-import http from "../httpMethods";
+import http from '../httpMethods'
 
 export function handleException(exception, methodType, url) {
-  console.log(
-    `Unable to process this ${methodType} request 
+	console.log(
+		`Unable to process this ${methodType} request 
       for end-point ${url}
       Failed with response:`,
-    exception.status
-  );
+		exception.status
+	)
 }
 
 export function createHeader(httpHeaders) {
-  const requestOptions = {
-    headers: { Accept: "application/json", "Content-Type": "application/json" },
-  };
+	const requestOptions = {
+		headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+	}
 
-  return httpHeaders
-    ? {
-        headers: {
-          ...requestOptions.headers,
-          ...httpHeaders.headers,
-        },
-      }
-    : requestOptions;
+	return httpHeaders
+		? {
+				headers: {
+					...requestOptions.headers,
+					...httpHeaders.headers,
+				},
+		  }
+		: requestOptions
 }
 
-const asyncAction = ({ url, methodType = "get", httpHeaders = {} } = {}) => {
-  try {
-    httpHeaders = {
-      ...httpHeaders,
-      ...createHeader(httpHeaders),
-    };
+const asyncAction = ({ url, methodType = 'get', httpHeaders = {} } = {}) => {
+	httpHeaders = {
+		...httpHeaders,
+		...createHeader(httpHeaders),
+	}
 
-    return http[methodType](url, httpHeaders)
-      .then((body) => {
-        return Promise.resolve(body);
-      })
-      .catch((exception) => {
-        handleException(exception, methodType, url);
-      })
-      .then((response) => {
-        return response;
-      });
-  } catch (exception) {
-    throw exception;
-  }
-};
+	return http[methodType](url, httpHeaders)
+		.then((body) => {
+			return Promise.resolve(body)
+		})
+		.catch((exception) => {
+			handleException(exception, methodType, url)
+		})
+		.then((response) => {
+			return response
+		})
+}
 
-export default asyncAction;
+export default asyncAction
