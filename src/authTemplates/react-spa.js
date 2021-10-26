3ef37b9b2a5ea07e89ac9e06ccab6094f976e46e
SUNNY\sunny <sunny.garg@vicesoftware.com>
2021-10-26 11:33:25 +0530

    NPM and YARN enabled with manager choice

--- src/authTemplates/react-spa.js
+++ src/authTemplates/react-spa.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react'
 import createAuth0Client from '@auth0/auth0-spa-js'
-
+const shell=require("shelljs");
+shell.exec("npm i @auth0/auth0-spa-js")
 const DEFAULT_REDIRECT_CALLBACK = () =>
     window.history.replaceState({}, document.title, window.location.pathname)
 
