5d587465a860ce55a994532ddc1fa5af758f0771
SUNNY\sunny <sunny.garg@vicesoftware.com>
2021-11-10 19:34:46 +0530

    Updated Reusable table component

--- src/templates/vue/src/components/organisms/Table.vue
+++ src/templates/vue/src/components/organisms/Table.vue
@@ -1,21 +1,32 @@
 <template>
   <div>
-    <b-table striped :items="allTodos" :fields="fields"></b-table>
+    <vue-bootstrap4-table v-bind:columns="columns" v-bind:rows="rows" :config="config">
+    </vue-bootstrap4-table>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import VueBootstrap4Table from "vue-bootstrap4-table";
 export default {
   name: "Table",
-  data() {
-    return {
-      fields: ["name", "username", "email"],
-      items: [],
-    };
-  },
-  computed: {
-    ...mapGetters(["allTodos"]),
+  props: ["columns", "rows"],
+   data: function() {
+            return {
+              config:{
+                card_mode:false,
+                pagination:false,
+                pagination_info:false,
+                highlight_row_hover:false,
+                global_search:{visibility:false},
+                show_refresh_button:  false,
+                show_reset_button:  false,
+                server_mode:  false,
+                preservePageOnDataChange: false
+              }
+            }
+            },
+  components: {
+    VueBootstrap4Table,
   },
 };
 </script>
