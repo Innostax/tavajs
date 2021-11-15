8ee67897a95ff13dfb0524eec1064ced183e7f8c
DESKTOP-DHVOT8D\himan <mayuri.singh@vicesoftware.com>
2021-11-15 10:57:49 +0530

    #50 Integrated Angular template as frontend

--- src/templates/angular/src/app/app.module.ts
+++ src/templates/angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrganismsComponent } from './components/organisms/organisms.component';
+import { UsersComponent } from './Screens/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganismsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
