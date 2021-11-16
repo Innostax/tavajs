764197199a08dbe4275681a6bb342c6322068cbe
Shahnoor Alam <shahnoor2607@gmail.com>
2021-11-16 17:14:57 +0530

    Implemented reusabe table vue js.

--- src/templates/vue/src/components/organisms/Table.vue
+++ src/templates/vue/src/components/organisms/Table.vue
@@ -1,32 +1,38 @@
 <template>
   <div>
-    <vue-bootstrap4-table v-bind:columns="columns" v-bind:rows="rows" :config="config">
-    </vue-bootstrap4-table>
+    <b-table :fields="fields" :items="items" striped>
+      <template v-for="user in allUsers" #cell(edit)="">
+        <b-button variant="primary" :key="user.id" v-on:click="$bvModal.show('bv-modal-editUser')" @click="editOne(user.id)"> Edit </b-button>
+      </template>
+      <template v-for="user in allUsers" #cell(delete)="">
+        <b-button variant="danger" :key="user.id" v-on:click="removeOne(user.id)"> Delete </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
-
 <script>
-import VueBootstrap4Table from "vue-bootstrap4-table";
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Table",
-  props: ["columns", "rows"],
-   data: function() {
-            return {
-              config:{
-                card_mode:false,
-                pagination:false,
-                pagination_info:false,
-                highlight_row_hover:false,
-                global_search:{visibility:false},
-                show_refresh_button:  false,
-                show_reset_button:  false,
-                server_mode:  false,
-                preservePageOnDataChange: false
-              }
-            }
-            },
   components: {
-    VueBootstrap4Table,
+  },
+  props: ["fields","items"],
+  methods: {
+    ...mapActions(["deleteUser", "selectedItem"]),
+    removeOne: function (id) {
+      this.deleteUser(id);
+    },
+    editOne: function (id) {
+      this.selectedItem(id);
+      this.modalShow = false;
+    },
+    
+  },
+  computed: {
+    ...mapGetters(["allUsers", "selectedUser"]),
+  },
+  created() {
+    this.mapActions;
   },
 };
 </script>
--- src/vuexTemplates/userModal/ShowUsers.vue
+++ src/vuexTemplates/userModal/ShowUsers.vue
@@ -1,37 +1,27 @@
 <template>
   <div>
-    <table width="100%" border="1px">
-      <tr>
-        <th>Name</th>
-        <th>User Name</th>
-        <th>Email</th>
-      </tr>
-      <tr v-for="user in allUsers" :key="user.id">
-        <td>{{ user.name }}</td>
-        <td>{{ user.username }}</td>
-        <td>{{ user.email }}</td>
-        <td>
-          <Button id="show-btn" className="mt-3" name="Delete" color="danger" @onClick="removeOne(user.id)" />
-        </td>
-        <td>
-          <Button id="show-btn" className="mt-3" name="Edit" color="primary" @submit="editOne(user.id)" @onClick="$bvModal.show('bv-modal-editUser')" />
-        </td>
-      </tr>
-    </table>
+    <Table :fields=columns :items=allUsers> </Table>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
-import Button from "../components/atoms/Button.vue";
+import Table from "../components/organisms/Table.vue";
 export default {
   name: "ShowUsers",
   components: {
-    Button,
+    Table,
   },
 
   data() {
     return {
+      columns:[
+        {key:"name" },
+        {key:"username"},
+        {key:"email"},
+        {key:"edit"},
+        {key:"delete"}
+      ],
       modalShow: true,
     };
   },
