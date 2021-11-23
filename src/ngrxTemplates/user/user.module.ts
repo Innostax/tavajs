cd788acf14b9718c9d56d7b7febf9994520d509b
Manjeet Beniwal <93973283+manjeetmirdha@users.noreply.github.com>
2021-11-23 13:27:34 +0530

    #69 integrate NgRxStore(state management library ) in angular (#71)

---
+++ src/ngrxTemplates/user/user.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserViewComponent } from './user-view/user-view.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import {StoreModule} from '@ngrx/store';
+import {userFeatureKey, reducer} from './store/reducer/user.reducer';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    UserViewComponent,
+    AddUserComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    StoreModule.forFeature(userFeatureKey, reducer),
+  ],
+  exports: [
+        UserViewComponent,
+        AddUserComponent
+      ]
+})
+export class UserModule { };
