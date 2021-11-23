3276f6377a7322a5d253511d65c89cdf7bae718a
Dinkar Tiwari <dinkar.tiwari@innostax.com>
2021-11-23 12:32:18 +0530

    resolved unwanted semicolons

--- src/ngrxTemplates/user/add-user/add-user.component.ts
+++ src/ngrxTemplates/user/add-user/add-user.component.ts
@@ -1,39 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/module/user';
-import { userState } from '../store/reducer/user.reducer';
-import { addUser } from '../store/action/user.actions';
-import { Store } from '@ngrx/store';
-import { FormGroup,FormBuilder,FormControl } from '@angular/forms';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/module/user";
+import { userState } from "../store/reducer/user.reducer";
+import { addUser } from "../store/action/user.actions";
+import { Store } from "@ngrx/store";
+import { FormGroup, FormBuilder, FormControl } from "@angular/forms";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-add-user',
-  templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.css']
-});
+  selector: "app-add-user",
+  templateUrl: "./add-user.component.html",
+  styleUrls: ["./add-user.component.css"],
+})
 export class AddUserComponent implements OnInit {
-  userForm: FormGroup ;
-  constructor(private store: Store<userState>,private modalService: NgbModal) {
-      };
-      open(addusermodal:any) {
-        this.modalService.open(addusermodal, {ariaLabelledBy: 'modal-basic-title'})   
-      } ;   
-      adduser(): void {
-        const user: User = {
-          id:this.userForm.value.id,
-          name: this.userForm.value.name,
-          username: this.userForm.value.username,
-          email: this.userForm.value.email,
-        };
-        this.store.dispatch(addUser({user}));
-        this.userForm.reset();
-      };
+  userForm: FormGroup;
+  constructor(
+    private store: Store<userState>,
+    private modalService: NgbModal
+  ) {}
+  open(addusermodal: any) {
+    this.modalService.open(addusermodal, {
+      ariaLabelledBy: "modal-basic-title",
+    });
+  }
+  adduser(): void {
+    const user: User = {
+      id: this.userForm.value.id,
+      name: this.userForm.value.name,
+      username: this.userForm.value.username,
+      email: this.userForm.value.email,
+    };
+    this.store.dispatch(addUser({ user }));
+    this.userForm.reset();
+  }
   ngOnInit(): void {
     this.userForm = new FormGroup({
-      id:new FormControl(),
-     name: new FormControl(),
-     username: new FormControl(),
-    email: new FormControl()
+      id: new FormControl(),
+      name: new FormControl(),
+      username: new FormControl(),
+      email: new FormControl(),
     });
-  };
-};
+  }
+}
--- src/ngrxTemplates/user/user-view/user-view.component.ts
+++ src/ngrxTemplates/user/user-view/user-view.component.ts
@@ -1,50 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
-import { User } from 'src/app/module/user';
-import {select, Store} from '@ngrx/store';
-import {selectusers} from '../store/selector/user.selectors';
-import {userState} from '../store/reducer/user.reducer';
-import { updateUser } from '../store/action/user.actions';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormGroup,FormControl } from '@angular/forms';
-import { deleteUser } from '../store/action/user.actions';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { User } from "src/app/module/user";
+import { select, Store } from "@ngrx/store";
+import { selectusers } from "../store/selector/user.selectors";
+import { userState } from "../store/reducer/user.reducer";
+import { updateUser } from "../store/action/user.actions";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { FormGroup, FormControl } from "@angular/forms";
+import { deleteUser } from "../store/action/user.actions";
 
 @Component({
-  selector: 'app-user-view',
-  templateUrl: './user-view.component.html',
-  styleUrls: ['./user-view.component.css']
-});
+  selector: "app-user-view",
+  templateUrl: "./user-view.component.html",
+  styleUrls: ["./user-view.component.css"],
+})
 export class UserViewComponent implements OnInit {
-  userForm: FormGroup ;
+  userForm: FormGroup;
   users$: Observable<User[]>;
-  constructor(private store: Store<userState>,private modalService: NgbModal) { 
+  constructor(private store: Store<userState>, private modalService: NgbModal) {
     this.users$ = this.store.pipe(select(selectusers));
-   };
-   open(editusermodal:any,user:User) {
-    this.modalService.open(editusermodal, {ariaLabelledBy: 'modal-basic-title'})
-    this.userForm.controls['name'].setValue(user.name)
-    this.userForm.controls['username'].setValue(user.username)
-    this.userForm.controls['email'].setValue(user.email)
-    this.userForm.controls['id'].setValue(user.id)
-  };
+  }
+  open(editusermodal: any, user: User) {
+    this.modalService.open(editusermodal, {
+      ariaLabelledBy: "modal-basic-title",
+    });
+    this.userForm.controls["name"].setValue(user.name);
+    this.userForm.controls["username"].setValue(user.username);
+    this.userForm.controls["email"].setValue(user.email);
+    this.userForm.controls["id"].setValue(user.id);
+  }
   edituser() {
     const user: User = {
-      id:this.userForm.value.id,   
+      id: this.userForm.value.id,
       name: this.userForm.value.name,
       username: this.userForm.value.username,
       email: this.userForm.value.email,
     };
-    this.store.dispatch(updateUser({user}));
-  };
-   deleteuser(id:string){
-    this.store.dispatch(deleteUser({id}))
-   }; 
+    this.store.dispatch(updateUser({ user }));
+  }
+  deleteuser(id: string) {
+    this.store.dispatch(deleteUser({ id }));
+  }
   ngOnInit(): void {
     this.userForm = new FormGroup({
-      id:new FormControl(),
+      id: new FormControl(),
       name: new FormControl(),
       username: new FormControl(),
-     email: new FormControl()
-     });
-  };
-};
+      email: new FormControl(),
+    });
+  }
+}
