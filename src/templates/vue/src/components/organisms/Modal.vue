09a600978509d025943cb2b7cbe6ecc4a6501602
Shahnoor Alam <shahnoor2607@gmail.com>
2021-11-25 12:20:29 +0530

    Created modal component and used in AddUser

---
+++ src/templates/vue/src/components/organisms/Modal.vue
@@ -0,0 +1,80 @@
+<template>
+  <b-modal :id="id" hide-footer>
+      <template #modal-title>{{title}}</template>
+      <div class="d-block text-center">
+        <b-row>
+          <b-col>
+            <Label name="Name:" />
+          </b-col>
+          <b-col>
+            <Input type="text" v-model="name" placeholder="Name" />
+          </b-col>
+        </b-row>
+        <br/>
+        <b-row>
+          <b-col>
+            <Label name="User Name:" />
+          </b-col>
+          <b-col>
+            <Input type="text" v-model="username" placeholder="Username" />
+          </b-col>
+        </b-row>
+        <br />
+        <b-row>
+          <b-col>
+            <Label name="Email:" />
+          </b-col>
+          <b-col>
+            <Input type="text" v-model="email" placeholder="example@email.com" />
+          </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+            <Button id="show-btn" className="mt-3" name="Add User" color="primary" @submit="onSubmit" @onClick="$bvModal.hide('bv-modal-addUser')" />
+          </b-col>
+        </b-row>
+      </div>
+  </b-modal>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import Button from "../atoms/Button.vue";
+import Label from "../atoms/Label.vue";
+import Input from "../atoms/Input.vue";
+export default {
+    name: "Modal",
+    props: ["id", "title"],
+    components: {
+    Button,
+    Label,
+    Input,
+    },
+    data() {
+    return {
+      name: null,
+      username: null,
+      email: null,
+    };
+  },
+  methods: {
+    ...mapActions(["addUser"]),
+    onSubmit() {
+      const data = {
+        name: this.name,
+        username: this.username,
+        email: this.email,
+      };
+      this.addUser(data);
+      this.name = "";
+      this.username = "";
+      this.email = "";
+    },
+  },   
+}
+</script>
+<style scoped>
+#app {
+  margin-bottom: "20px";
+}
+</style>
