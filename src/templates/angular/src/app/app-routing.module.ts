387dab623e603593a3aa9cb330b951af1240a9a4
Rajat <rajat.verma@innostax.com>
2022-09-13 16:03:12 +0530

    Update structure for app component. Added generic header component in the pages folder.

--- src/templates/angular/src/app/app-routing.module.ts
+++ src/templates/angular/src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { UsersComponent } from './pages/users/users.component';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'users', component: UsersComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
