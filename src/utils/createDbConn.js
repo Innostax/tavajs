95cea2b2700c2db5a150919a96818ebc4d2864ac
Manjeet Beniwal <manjeet.singh@innostax.com>
2022-09-19 18:31:54 +0530

    Resolved comments

--- src/utils/createDbConn.js
+++ src/utils/createDbConn.js
@@ -1,27 +1,29 @@
 const fs = require("fs");
 const { updateProjectDependencies } = require("./helper");
 const { render } = require("ejs");
+const { DATABASES } = require("../constants");
+const { POSTGRES, MYSQL } = DATABASES;
 //<----------------------------- Function to create db service -------------------------------------------->
 function createDbConn(nodePath, dbName, defaultRoute, currentPath) {
-  const dependencies = [];
+  let dependencies = [];
   let fileName;
   let modelName;
   const modelPath = nodePath + "/models";
-  const isPostgres = dbName === "postgres";
-  const isMySQL = dbName === "mysql";
 
-  if ( isPostgres || isMySQL) {
-    dependencies.push({ name: "sequelize", version: "^6.6.5" });
-    fileName = "sequelize.js";
-    modelName = "sequelizeModel.js";
-
-    isMySQL
-      ? dependencies.push({ name: "mysql2", version: "^2.3.0" })
-      : dependencies.push({ name: "pg", version: "^8.7.1" });
-  } else {
-    dependencies.push({ name: "mongoose", version: "^6.0.2" });
-    fileName = "mongoose.js";
-    modelName = "mongooseModel.js";
+  switch (dbName) {
+    case POSTGRES:
+    case MYSQL:
+      fileName = "sequelize.js";
+      modelName = "sequelizeModel.js";
+      dependencies = [...dependencies, { name: "sequelize", version: "^6.6.5" }]
+      if (MYSQL) dependencies.push({ name: "mysql2", version: "^2.3.0" });
+      else dependencies = [...dependencies, { name: "pg", version: "^8.7.1" }];
+      break;
+    default:
+      fileName = "mongoose.js";
+      modelName = "mongooseModel.js";
+      dependencies = [...dependencies, { name: "mongoose", version: "^6.0.2" }];
+      break;
   }
 
   // Updating package dependencies
@@ -29,29 +31,24 @@ function createDbConn(nodePath, dbName, defaultRoute, currentPath) {
 
   // Create modal directory
   fs.mkdirSync(modelPath);
-
   let databaseFilePath = `${nodePath}/${fileName}`;
-
   // Reading Database file data
   let databaseFile = fs.readFileSync(
     currentPath + `/dbTemplates/` + fileName,
     "utf8"
   );
   databaseFile = render(databaseFile, { defaultRoute });
-
   // Writing database file data
   fs.writeFileSync(databaseFilePath, databaseFile, "utf8");
 
   // Database file path
   databaseFilePath = `${modelPath}/${defaultRoute}.js`;
-
   // // Reading Database file data
   databaseFile = fs.readFileSync(
     currentPath + `/dbTemplates/` + modelName,
     "utf8"
   );
   databaseFile = render(databaseFile, { defaultRoute });
-
   // Writing database file data
   fs.writeFileSync(databaseFilePath, databaseFile, "utf8");
 }
