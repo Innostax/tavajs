e3478441d5f749073e1a382b51c3c53de7d146af
Manjeet Beniwal <93973283+manjeetbeniwalinnostax@users.noreply.github.com>
2022-09-21 14:23:28 +0530

    fix-issues-in-model-footer-and-close-buttons (#196)

--- src/templates/react/src/components/bootstrap/organisms/Modal.js
+++ src/templates/react/src/components/bootstrap/organisms/Modal.js
@@ -12,14 +12,13 @@ export default function ViceModal({
 	
 	return (
 		<Modal show={show} onHide={handleClose} size={size}>
-			<Modal.Header className='border-0'>
-				<Modal.Title className='f-32 font-semi-bold'>{title}</Modal.Title>
-				<button className='btn-close' onClick={handleClose}></button>
-			</Modal.Header>
-			<Modal.Body className='py-0 mb-0'>{children}</Modal.Body>
-			<Modal.Footer className='justify-content-between border-0'>
-				{footer}
-			</Modal.Footer>
-		</Modal>
+            <Modal.Header closeButton className='border-0'>
+                <Modal.Title className='f-32 font-semi-bold'>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className='py-0 mb-0'>{children}</Modal.Body>
+            <Modal.Footer className='justify-content-between border-0'>
+                {footer}
+            </Modal.Footer>
+        </Modal>
 	)
 }
