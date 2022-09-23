2deec177cbf1717d716fce3630e35299dd7d105a
Manjeet Beniwal <manjeet.singh@innostax.com>
2022-09-23 15:16:05 +0530

    Removed commented code

--- src/utils/handleRenderEJS.js
+++ src/utils/handleRenderEJS.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const { readFile } = require("./helper");
 const { render } = require("ejs");
 
-const handleRenderEJS = (readFilePath, props, filePath) => {
+const handleRenderEJS = (readFilePath, props, writeFilePath) => {
   let dockerFile = readFile(readFilePath);
   dockerFile = render(dockerFile, { ...props });
-  fs.writeFileSync(filePath, dockerFile, "utf8");
+  fs.writeFileSync(writeFilePath, dockerFile, "utf8");
 };
 
 module.exports = { handleRenderEJS };
