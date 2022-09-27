05601c85fb1e1cc2dfdf2d41f17a37c684284274
Rajat Verma <101191176+rajat-vrma@users.noreply.github.com>
2022-09-27 14:43:05 +0530

    232/Integrate sendgrid in nodejs (#247)

--- src/emailTemplates/sendgrid.js
+++ src/emailTemplates/sendgrid.js
@@ -1,20 +1,17 @@
-require("dotenv").config();
-const sgMail=require('@sendgrid/mail');
-
-sendMail=(to,from,text,html,subject)=>{
-    sgMail.setApiKey=process.env.ApiKey;
-    
-    try {
-        const info = sgMail.send({
-          from :from, // sender address
-          to:to, // list of receivers
-          subject:subject,
-          text:text,
-          html:html,
-        })
-        return info
-      } catch (err) {
-        console.log(err.response, 'error on sending mail.')
-      }
-    }
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const sendMail = () => {
+  const msg = {
+    to: 'Change to your recipient',
+    from: 'Change to your sendgrid verified sender',
+    subject: 'enter mail subject here',
+    text: 'Enter mail Text',
+    html: 'Enter your html here',
+  }
+  sgMail
+    .send(msg)
+    .then(() => console.log('Email sent'))
+    .catch((error) => console.error(error))
+}
+module.exports = { sendMail }
 
