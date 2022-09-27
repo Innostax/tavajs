0cec46fcbfbfd6295170bdeb31a09a57e8c43e7d
Manjeet Beniwal <manjeet.singh@innostax.com>
2022-09-27 12:44:38 +0530

    Refactored changes

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,9 +1,8 @@
-
 const { handleAnswersEvaluator } = require("../src/answersEvaluator");
-describe('mock usage', () => {
-    it('should use the mock', async () => {
-      const result = await handleAnswersEvaluator();
-      console.log("result ---->", result)
+describe("mock usage", () => {
+  it("should use the mock", async () => {
+    const result = await handleAnswersEvaluator();
+    console.log("result ---->", result);
     //   expect(result).toBe('test@test.com');
-    })
   });
+});
