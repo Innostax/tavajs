7119e724f7d2f69d221bd06bb0e66af7c6440b0e
Rajat Verma <101191176+rajat-vrma@users.noreply.github.com>
2022-09-29 18:35:38 +0530

    275/Refactored angular application (#278)

--- src/ngrxTemplates/user-actions-modal/user-actions-modal.component.ts
+++ src/ngrxTemplates/user-actions-modal/user-actions-modal.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, OnChanges, Output, Input, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+<% if(isCrud){%>
 import { Store } from "@ngrx/store";
 import { addUser, updateUser } from 'src/app/utils/store/action/user.actions';
 import { userState } from 'src/app/utils/store/reducer/user.reducer';
 import {User} from 'src/app/utils/store/User';
+<%}%>
 import { v4 as uuid } from 'uuid';
+<% if(isCrudWithNode){%>import { ApiService } from 'src/app/shared/services/services'; <%}%>
 
 
 @Component({
@@ -18,13 +21,13 @@ export class UserActionsModalComponent implements OnInit, OnChanges {
   @Output() closeEvent: EventEmitter<any> = new EventEmitter<any>();
   @Output() performDeleteAction: EventEmitter<any> = new EventEmitter<any>();
   createUserForm!: FormGroup;
-  user!: User;
+  <% if(isCrud){%> user!: User; <%}%>
   userActionLabel: string = 'Add';
 
   get registerFormControl() {
     return this.createUserForm.controls;
   }
-  constructor( private fb: FormBuilder,  private store: Store<userState>, ) {  }
+  constructor( private fb: FormBuilder,  <% if(isCrud){%> private store: Store<userState>,  <%}%> <% if(isCrudWithNode){%> private apiService: ApiService<%}%> ) {  }
 
   ngOnInit(): void {
     this.initForm()
@@ -58,9 +61,15 @@ export class UserActionsModalComponent implements OnInit, OnChanges {
       email: this.createUserForm.get('email')?.value
     }
 
-    if(this.data?.name == '') this.store.dispatch(addUser({user: userData}))
-    else this.store.dispatch(updateUser({user: userData}))
+    if(this.data?.name == '') <% if(isCrud){%>this.store.dispatch(addUser({user: userData}))
+    else this.store.dispatch(updateUser({user: userData})) <%}%>
+    <% if(isCrudWithNode){%> this.apiService.createEmployee(userData).subscribe((res)=>{})
+    else this.apiService.updateEmployee(this.data.id,userData).subscribe((res)=>{}) <%}%>
     this.createUserForm.reset();
+    <% if(isCrudWithNode){%>
+     // To-Do: Need to update api call for get users
+     this.refreshView();
+     <%}%>
     this.closeModalRef();
   }
 
@@ -73,4 +82,9 @@ export class UserActionsModalComponent implements OnInit, OnChanges {
     this.closeEvent.emit();
     this.createUserForm.reset();
   }
+  <% if(isCrudWithNode){%>
+  refreshView() {
+    window.location.reload();
+  }
+  <%}%>
 }
