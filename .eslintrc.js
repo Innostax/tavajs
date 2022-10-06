a8786f752ee86c99f141a8f2f69e01db93eeac7c
Himanshu  Mishra <himanshu.mishra@innostax.com>
2022-10-06 18:11:56 +0530

    eslint-changes

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true, 
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended", 
+        "plugin:jest/recommended",
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    root: true,
+    "rules": {
+        quotes: [
+            "warn",
+            "double"
+        ],
+        indent: [
+            "error",
+            4
+        ],
+        semi: [
+            "error",
+            "always"
+        ],
+        
+    }
+};
