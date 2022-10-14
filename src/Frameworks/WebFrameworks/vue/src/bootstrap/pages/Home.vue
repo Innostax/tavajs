ca578eda999ab1de9f47e338014498cd62712927
Rajat Verma <101191176+rajat-vrma@users.noreply.github.com>
2022-10-14 18:11:39 +0530

    373/Implement generate new screen for frontend application (#375)

--- src/Frameworks/WebFrameworks/vue/src/bootstrap/pages/Home.vue
+++ src/Frameworks/WebFrameworks/vue/src/bootstrap/pages/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <% if (isOkta) { %>
   <div v-if="authState && authState.isAuthenticated">
-    <% } else { %>
+  <% } else { %>
   <div>
-    <% } %>
+  <% } %>
     <h1>Welcome to Home Screen</h1>
   </div>
 </template>
